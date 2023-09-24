969b2fbb0e45e91667b0b285bcc57eddcd6e0280
syazyuuwan <nawsay7@gmail.com>
2023-09-24 09:43:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ with open('styles.css') as f:
 def main():
     LOGO_IMAGE_URL = "https://raw.githubusercontent.com/syazyuuwan/YP_AI05_OpenAI_API_Project/main/images/Prepfection_mix_50pct.png"
     
-    st.markdown(f"![Logo]({LOGO_IMAGE_URL})")
+    st.image(LOGO_IMAGE_URL, caption='Logo', use_column_width=True)
     st.markdown("""
     <div class="logo-container">
         <p>An AI-Powered Hyper-Customisable Meal Prep Tool</p>
